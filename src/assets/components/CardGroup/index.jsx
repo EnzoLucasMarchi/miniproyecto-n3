3505753c32d6c11625429f68fa8dd42089184f99
Enzo <enzolucasmarchi@gmail.com>
2024-05-20 15:20:57 -0300

    fuente y estilos menores

--- src/assets/components/CardGroup/index.jsx
+++ src/assets/components/CardGroup/index.jsx
@@ -2,15 +2,13 @@ import { Card } from './Card'
 import './CardGroup.css'
 
 const CardGroup = ({data, filter, setFilter}) => {
-    const maxCards = 6;
-    const moreStays = filter.length > maxCards ? filter.length - maxCards : 0;
     return (
         <>
             <div className="cardgroup-container">
                 <div className='section-header'>
                 {data.length && data[0]?.country && <h2> Stay in {data[0].country}</h2>}
                 {/* ojaldre con esta linea*/}
-                <p>{moreStays > 0 ? `${moreStays}+stays` : ''}</p>
+                <p>{`${data.length}+stays`}</p>
                 </div>
                 <div className="cards-area">
                     <ul className="card-list">
