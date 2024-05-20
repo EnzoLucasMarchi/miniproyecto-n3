8c32b668403760ac6b49f9a09520c28e08feb029
Enzo <enzolucasmarchi@gmail.com>
2024-05-20 10:53:02 -0300

    estilos: quedo feo igual xd

--- src/assets/components/CardGroup/index.jsx
+++ src/assets/components/CardGroup/index.jsx
@@ -7,9 +7,11 @@ const CardGroup = ({data, filter, setFilter}) => {
     return (
         <>
             <div className="cardgroup-container">
-                 {data.length > 0 && data[0] && data[0].country && <h2> Stay in {data[0].country}</h2>}
+                <div className='section-header'>
+                {data.length && data[0]?.country && <h2> Stay in {data[0].country}</h2>}
                 {/* ojaldre con esta linea*/}
                 <p>{moreStays > 0 ? `${moreStays}+stays` : ''}</p>
+                </div>
                 <div className="cards-area">
                     <ul className="card-list">
                         {
